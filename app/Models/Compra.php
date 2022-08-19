cda57e424b1c2d53ce7deff24bc829357888bffa
cacimboangola <arnaldo.cacimbo@gmail.com>
2022-08-19 13:27:57 +0100

    Add Services Directory

--- app/Models/Compra.php
+++ app/Models/Compra.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Compra extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'data',
+        'total',
+        'user_id'
+    ];
+
+    public function user(){
+        $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function getUserNameAttribute(){
+        return $this->user()->name;
+    }
 }
